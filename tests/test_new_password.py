4b4e1a44dccb4459668b321a32789dcd6378c7b0
BANDITOS312 <som.brn@gmail.com>
2025-03-16 16:52:45 +0700

    Добавил тест на проверку длины пароля

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,7 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
-
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_password_len():
+    """Тест на длину пароля"""
+    password = generate_password(100)
+    assert len(password) == 100
