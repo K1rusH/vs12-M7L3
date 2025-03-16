7d2d32e5d63d17d543588814bdb60e1087220b79
amogus213224 <denisartuskin901@gmail.com>
2025-03-16 14:07:47 +0400

    Обновлен тест функционала пароля: проверка на длинну пароля и на уникальность каждого пароля

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -7,6 +7,20 @@ def test_password_characters():
     password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
     for char in password:
         assert char in valid_characters
+    
+def test_pass_leng():
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    passleng = len(str(password))
+    assert passleng == 100
+
+def test_pass_same():
+    valid_characters = string.ascii_letters + string.digits + string.punctuation
+    password1 = generate_password(100)  # Генерируем длинный пароль для более надежной проверки
+    password2 = generate_password(100)
+    password3 = generate_password(100)
+    password4 = generate_password(100)
+    assert not (password1 == password2 and password1 == password3 and password1 == password4)
 
 """
 Допиши еще один тест из предложенных. Или придумай свой.
