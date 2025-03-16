4e4e4c5e5c81485e69fad6ef9631c93f5b6ae706
Volkobuv <volkobdaniil.2010@gmail.com>
2025-03-16 20:20:39 +1000

    РПНПРГРО

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,10 +8,14 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
-"""
-Допиши еще один тест из предложенных. Или придумай свой.
-Если сможешь написать больше, то будет круто!
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    length = 16
+    password = generate_password(length)
+    assert len(password) == length, f"Длина пароля должна быть {length}, но получена {len(password)}"
 
-Тест, что длина пароля соответствует заданной
-Тест, что два сгенерированных подряд пароля различаются
-"""
+def test_unique_passwords():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(12)
+    password2 = generate_password(12)
+    assert password1 != password2, "Сгенерированные пароли должны различаться"
